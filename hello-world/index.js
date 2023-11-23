f3060528ba1f3ad1fce47117b0b615ad3eb15329
Kelvin Jesus <kelvin.jesus@webkriativa.com.br>
2023-11-23 16:54:35 -0300

    update index.html and create main.js to export common functions

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,3 +1,5 @@
+import { go, wasmBrowserInstantiate } from "../main";
+
 document.addEventListener('DOMContentLoaded', () => {
     const $xInput = document.querySelector('.x');
     const $yInput = document.querySelector('.y');
@@ -12,33 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-export const wasmBrowserInstantiate = async (wasmModuleURL, importObject) => {
-    let response;
-
-    // dont have support for streaming instantiation
-    if ( !WebAssembly.instantiateStreaming ) {
-        // download its entire module then instantiate it
-        const fetchAndInstantiateTask = async () => {
-            const wasmArrayBuffer = await fetch(wasmModuleURL)
-                .then(response => response.arrayBuffer());
-            
-            return WebAssembly.instantiate(wasmArrayBuffer, importObject);
-        }
-
-        response = await fetchAndInstantiateTask();
-        return response;
-    }
-
-    // fetch the wasm module via streaming and instantiate it as it is downloading
-    response = await WebAssembly.instantiateStreaming(
-        fetch(wasmModuleURL),
-        importObject
-    );
-
-    return response;
-}
-
-const go = new Go(); // defined in wasm_exec
 const importObject = go.importObject;
 
 // Instantiate the wasm module
